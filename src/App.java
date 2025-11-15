88fcedb9b5cee5f8cf5870c59a2d52e184413e22
Curro Portillo <211curroportillo@gmail.com>
2025-11-15 13:33:44 +0100

    ejercicio 60

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de los calcetines: "));
+        int base = 1;
+
+        while(base <= altura){
+            if(base > altura-2){
+                System.out.println("******  ******");
+                
+            }else{
+                System.out.println("***     ***");    
+            }
+            base++;
+        }    
     }
 }
